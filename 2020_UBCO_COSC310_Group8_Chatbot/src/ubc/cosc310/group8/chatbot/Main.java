2b2fa2233d6f25a49c0d1aaf25d2202adb8a9056
A-Nixon <>
2020-02-13 08:33:30 -0800

    02/13/2020 Made main method and added loop to iterate over args[]

---
+++ 2020_UBCO_COSC310_Group8_Chatbot/src/ubc/cosc310/group8/chatbot/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ubc.cosc310.group8.chatbot;
+
+/**
+ *
+ * @author A
+ */
+public class Main {
+    public static void Main(String[] args){
+        //Read in command line arguments
+        for(int i = 0; i < args.length; i++){ //not using enhanced loop so that we can manually increase i to handle flag variables. e.g. -flagthattakesanumber 3
+            
+        }
+    }
+}
