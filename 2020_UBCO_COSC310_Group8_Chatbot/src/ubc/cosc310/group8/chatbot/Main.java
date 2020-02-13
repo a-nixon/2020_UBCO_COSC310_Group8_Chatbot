f46997855e43ab4a4b1f777bae07b428a32a0abe
A-Nixon <>
2020-02-13 08:33:30 -0800

    02/13/2020 Added a switch statement to the argument handling portion of the main method.

--- 2020_UBCO_COSC310_Group8_Chatbot/src/ubc/cosc310/group8/chatbot/Main.java
+++ 2020_UBCO_COSC310_Group8_Chatbot/src/ubc/cosc310/group8/chatbot/Main.java
@@ -12,8 +12,11 @@ package ubc.cosc310.group8.chatbot;
 public class Main {
     public static void Main(String[] args){
         //Read in command line arguments
-        for(int i = 0; i < args.length; i++){ //not using enhanced loop so that we can manually increase i to handle flag variables. e.g. -flagthattakesanumber 3
-            
+        for(int i = 0; i < args.length; i++){ //not using enhanced loop so that we can manually increase i to handle flag variables. e.g. -flagthattakesanumber 3 -> if args[i].equals("-flagthattakesanumber"){i++; intVar = Integer.parseInt(args[i]);}
+            switch(args[i]){
+                default:
+                    break;
+            }
         }
     }
 }
