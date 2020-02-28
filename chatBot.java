c52039f181011a362797ddb1e0828ce3a1a6c40f
Coleton Weninger <coletonw2@hotmail.com>
2020-02-27 16:36:41 -0800

    chatBot organization Parses question words

--- chatBot.java
+++ chatBot.java
@@ -2,15 +2,12 @@ import java.util.Scanner;
 public class chatBot{
     private String role;
     private String userName;
+    private String question;
     
     public chatBot(){
         this.role = "Call Center Agent";
-        
-        Scanner input = new Scanner(System.in);
-        System.out.println("Hello, my name is chatBot. May I start with your name?");
-        this.userName = input.nextLine();
-        System.out.println("Hello " + userName + " How may I help you?");
-        
+        init();
+
             
     }
 
@@ -19,5 +16,15 @@ public class chatBot{
     }
     public void setRole(String s){
         this.role = s;
+    }
+
+    public void init(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, my name is chatBot. May I start with your name?");
+        this.userName = input.nextLine();
+        System.out.println("Hello " + userName + " How may I help you?");
+        this.question = input.nextLine();
+        stringParse.parse(question);
+
     }
 }
--- stringParse.java
+++ stringParse.java
@@ -4,7 +4,26 @@ public class stringParse{
         String sentence = input;
         String response = "";
         String[] parsed = sentence.split(" ");
+        String qWord = parsed[0];
 
+        if(qWord.equalsIgnoreCase("who")){
+            //System.out.println("This is a who question");
+        }
+        if(qWord.equalsIgnoreCase("what")){
+            //System.out.println("This is a what question");
+        }
+        if(qWord.equalsIgnoreCase("where")){
+            //System.out.println("this is a where question");
+        }
+        if(qWord.equalsIgnoreCase("when")){
+            //System.out.println("This is a when question");
+        }
+        if(qWord.equalsIgnoreCase("why")){
+            //System.out.println("this is a why question");
+        }
+        if(qWord.equalsIgnoreCase("how")){
+            //System.out.println("this is a how question");
+        }
 
         return response;
     }
